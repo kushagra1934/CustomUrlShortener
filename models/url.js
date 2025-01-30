73387fd5c413f669bce12713d770bdfa9cb4edbb
Kushagra Saxena <kushagra.2022ug1093@iiitranchi.ac.in>
2025-01-30 22:08:53 +0530

    files

---
+++ models/url.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+//defining the schema
+const urlSchema = new mongoose.Schema(
+  {
+    shortId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    redirectURL: {
+      type: String,
+      required: true,
+    },
+    visitHistory: [
+      {
+        timestamp: { type: Number },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const URL = mongoose.model("url", urlSchema);
+
+//export
+module.exports = URL;
