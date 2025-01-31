ef6181a2cbcbd685cd0a6264cd9f94d97878324e
Kushagra Saxena <kushagra.2022ug1093@iiitranchi.ac.in>
2025-01-31 19:51:01 +0530

    auth-scratch

--- models/url.js
+++ models/url.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 
-//defining the schema
 const urlSchema = new mongoose.Schema(
   {
     shortId: {
@@ -12,16 +11,15 @@ const urlSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    visitHistory: [
-      {
-        timestamp: { type: Number },
-      },
-    ],
+    visitHistory: [{ timestamp: { type: Number } }],
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+    },
   },
   { timestamps: true }
 );
 
 const URL = mongoose.model("url", urlSchema);
 
-//export
 module.exports = URL;
